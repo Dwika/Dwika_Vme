20caded2fb13196b7f34b4c5ee79d8f8712545dc
Dimas Putra <dimas.dwikaputra@gmail.com>
2013-10-21 23:17:56 +1100

    Add extensions file to git for starter

---
+++ app/code/local/Dwika/Vme/Helper/Data.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Dwika Vme Extensions for Visa Vme Payment Method
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is Open Source License under GPL v2.
+ * 
+ * It is free and available to download and customise on your own needs
+ * this code is managed by Dwika.org and source available on github:
+ * https://github.com/Dwika/Dwika_Vme
+ * 
+ * If you wish to contribute on development, FIPR (Fork It and Pull Request!)
+ *
+ * @category    Dwika
+ * @package     Dwika_Vme
+ * @copyright   Copyright (c) 2013 Dwika.org (http://www.dwika.org)
+ * @license     https://github.com/Dwika/Dwika_Vme/blob/master/LICENSE - GPL v2
+ * @author      Dimas Putra - dimas.putra@dwika.com.au
+ */
+
+class Dwika_Vme_Helper_Data extends Mage_Core_Helper_Data
+{
+    // return all parents
+}
